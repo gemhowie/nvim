51acb14b19751864fd00aafa6e5bd7173b2d3acc
howie <howie.wei@outlook.com>
2024-06-04 00:52:54 +0800

    update plugins config, extra config

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,25 +1,19 @@
 return {
   {
     "linux-cultist/venv-selector.nvim",
-    -- cmd = { "VenvSelect", "VenvSelectCached", "VenvSelectCurrent" },
-    opts = function(_, opts)
-      if require("lazyvim.util").has("nvim-dap-python") then
-        opts.dap_enabled = true
-      end
-      return vim.tbl_deep_extend("force", opts, {
-        name = {
-          "venv",
-          ".venv",
-          "env",
-          ".env",
-          "nvim_venv",
-        },
-      })
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+      require("venv-selector").setup()
     end,
     keys = {
-      { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" },
-      { "<leader>cc", "<cmd>:VenvSelectCached<cr>", desc = "Select VirtualEnv Cached" },
-      -- { "<leader>cn", "<cmd>:VenvSelectCurrent<cr>", desc = "Current Select VirtualEnv" },
+      { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "Select VirtualEnv" },
     },
   },
 }
